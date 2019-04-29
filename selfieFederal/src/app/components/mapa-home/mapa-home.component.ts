a7be344c344d91b40cb4e1a4bc948855a2b195b4
Valentina <valentinaoppen@gmail.com>
2019-04-29 13:15:14 -0300

    div Mapa

---
+++ selfieFederal/src/app/components/mapa-home/mapa-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mapa-home',
+  templateUrl: './mapa-home.component.html',
+  styleUrls: ['./mapa-home.component.css']
+})
+export class MapaHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
