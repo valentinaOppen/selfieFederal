4e2ed8c44ae3670ca8d00b5f531aad066eb7ce0f
Valentina <valentinaoppen@gmail.com>
2019-04-29 13:35:37 -0300

    div mapa

--- selfieFederal/src/app/app.module.ts
+++ selfieFederal/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MapaHomeComponent } from './components/mapa-home/mapa-home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MapaHomeComponent
+    MapaHomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
