586255e205a5b0796335f4c5b42724e92dc9141d
Mario <mariorampi@gmail.com>
2019-04-30 23:53:47 -0300

    Inicio maps

--- selfieFederal/src/app/app.module.ts
+++ selfieFederal/src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
+
+import { MapsService } from './services/maps.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MapsComponent } from './components/maps/maps.component';
+
+import { MarkerInfoComponent } from "./components/maps/marker-info/markerInfoComponent";
+import { MapMarkersComponent } from './components/maps/map-markers/map-markers.component';
+import { MapClickComponent } from './components/maps/map-click/map-click.component';
+import { MapComponent } from './components/maps/map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MapsComponent,
+    MarkerInfoComponent,
+    MapMarkersComponent,
+    MapClickComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAtgzQpQEVf70TLbg_UAy0L5CcEYNRdw1w'
+    })
   ],
-  providers: [],
+  providers: [
+    GoogleMapsAPIWrapper,
+    MapsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ selfieFederal/src/app/services/maps.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { 
+	// GoogleMapsAPIWrapper,
+	MapsAPILoader, } from '@agm/core/services';
+import { Icoords,
+	// Imarker,
+	ILatLng,
+	ILocation as Location } from '../components/maps/imaps';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class MapsService {
+
+	private geocoder;
+	constructor(public mapsApiLoader: MapsAPILoader) {
+		this.mapsApiLoader.load().then(() => {
+			this.geocoder = new google.maps.Geocoder();			
+		});
+	}
+
+	findAddressByCoordinates(coords: ILatLng) {
+		
+
+		return new Promise((resolve, reject) => {
+			this.geocoder.geocode({
+				'location': {
+					lat: coords.lat,
+					lng: coords.lng
+				},
+				language: 'es-419',
+				// bounds: 'AR',
+				// componentRestrictions: ['administrativeArea'],
+				region: 'AR'
+			}, (results, status) => {
+				console.log(results, status)
+				resolve({
+					status, address: this.decomposeAddressComponents(results), ...coords
+				});
+			})
+		})
+	}
+
+	decomposeAddressComponents(addressArray) {
+		if (addressArray.length == 0) return false;
+		let address = addressArray[0].address_components;
+		let location = {};
+		for (let element of address) {
+			if (element.length == 0 && !element['types']) continue
+
+			if (element['types'].indexOf('street_number') > -1) {
+				location['address_level_1'] = element['long_name'];
+				continue;
+			}
+			if (element['types'].indexOf('route') > -1) {
+				location['address_level_1'] += ', ' + element['long_name'];
+				continue;
+			}
+			if (element['types'].indexOf('locality') > -1) {
+				location['address_level_2'] = element['long_name'];
+				continue;
+			}
+			if (element['types'].indexOf('administrative_area_level_1') > -1) {
+				location['address_state'] = element['long_name'];
+				continue;
+			}
+			if (element['types'].indexOf('country') > -1) {
+				location['address_country'] = element['long_name'];
+				continue;
+			}
+			if (element['types'].indexOf('postal_code') > -1) {
+				location['address_zip'] = element['long_name'];
+				continue;
+			}
+		}
+
+		return location;
+	}
+}
