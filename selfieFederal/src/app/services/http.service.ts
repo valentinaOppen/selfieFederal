9d3e880ee47df73b8b415226fc4d20f40065a7a8
mario <mariorampi@gmail.com>
2019-05-15 11:10:33 -0300

    Legales

--- selfieFederal/src/app/services/http.service.ts
+++ selfieFederal/src/app/services/http.service.ts
@@ -6,7 +6,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 	providedIn: 'root'
 })
 export class HttpService {
-	URL = 'https://selfiefederal.com/api/';// 
+	URL = 'https://selfiefederal.com/api/';
 	// URL = "http://127.0.0.1:8080/selfieFederal";
 	httpOptions = {
 		headers: new HttpHeaders({
